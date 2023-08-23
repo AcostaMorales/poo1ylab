d2f7ec4119c569258173a83096378fb743bb5ec4
AcostaMorales <acostamoralespablodaniel@gmail.com>
2023-08-23 09:14:00 -0600

    Actividad 2

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 - _p03_AreaTriangulo
 - _p04_PagaTrabajador
   
+## Actividad 2
+- _p05_OperacionesMatematicas
+- _p06_PromedioCalificaciones
+- _p07_OperadoresAsignacion
+- _p08_FuncionesTrigonometricas
+- _p09_DividirEnCifras
---
+++ _p08_FuncionesTrigonometricas.java
@@ -0,0 +1,20 @@
+// Calcular funciones trigonometricas de un triangulo en radianes
+
+import java.util.Scanner;
+
+public class _p08_FuncionesTrigonometricas {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");System.out.flush();
+
+        System.out.print("Dame un angulo en Grados: ");
+        double angulog = new Scanner(System.in).nextDouble();
+        double angulor = Math.toRadians(angulog);
+
+        double sen = Math.sin(angulor);
+        double cos = Math.cos(angulor);
+        double tan = Math.tan(angulor);
+
+        System.out.println(String.format("El angulo es: %.2f\nSeno: %.2f\nCoseno: %.2f\nTangente: %.2f",angulor, sen, cos, tan));
+
+    }    
+}
